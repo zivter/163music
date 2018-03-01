7cad00b45b5fb04d51450a6319f9cb65e873ea1b
zivter <yezhouteng@gmail.com>
2018-03-01 10:18:07 +0800

    修改搜索

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <div class="searchbox">
-            <input type="text" placeholder="搜索歌曲、歌单、专辑" class="search" v-model="sub" @keyup.enter="submit()" @click="show()"/>
+            <input type="text" placeholder="搜索歌曲、歌单、专辑" class="search" 
+            v-model="sub" @keyup.enter="submit()" @click="show()"/>
             <span class="cancle" v-show="isshow" @click="showup()">取消</span>
         </div>
         <div class="resou" v-show="isresou">
             <p class="remen">热门搜索</p>
             <ul class="searchul">
-                <li v-for="search,index in searchlist"  v-if="index<=11">{{search.k}}</li>
+                <li v-for="search,index in searchlist" @click="searchThis(search.k)" v-if="index<=11">{{search.k}}</li>
             </ul>
         </div>
         <div class="sousuo" v-show="issousuo">
@@ -18,7 +19,6 @@
                         <p class="songname">{{search1.songname}}</p>
                         <p class="singer">{{search1.singer[0].name}}</p>
                     </div>
-                   
                 </li>
             </ul>
         </div>
@@ -68,8 +68,17 @@ export default{
             this.issousuo = false;
             this.sub = "";
             this.searchlist1 = [];
+        },
+        searchThis(e){
+            axios.get(`/soso/fcgi-bin/search_for_qq_cp?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&w=${e}&zhidaqu=1&catZhida=1&t=0&flag=1&ie=utf-8&sem=1&aggr=0&perpage=20&n=20&p=1&remoteplace=txt.mqq.all&_=1510970390702`,{
+            }).then(res=>{
+                console.log(res.data.data.song.list);
+                this.searchlist1 = res.data.data.song.list;
+            }).then(res=>{
+                this.sub = e;
+                this.show();
+            });
         }
-
     }
 }
 </script>
@@ -78,16 +87,20 @@ export default{
     background: #000 !important;
 }
 .remen{
-    margin: 10px 10px;
+    margin: .2rem .3rem;
+    font: 700 .5rem/.8rem "";
+}
+.searchul{
+    margin-left: .2rem;
 }
 .searchul li{
     float: left;
-    padding:3px 10px;
+    padding:.06rem .1rem;
     border: 1px solid #666;
     list-style: none;
-    margin:6px 8px;
-    border-radius: 99px;
-    font-size: 15px;
+    margin:.1rem .08rem;
+    border-radius: .1rem;
+    font-size: .28rem;
 }
 .searchul li:nth-child(1),.searchul li:nth-child(2){
     color:#d43c33;
@@ -97,41 +110,43 @@ export default{
     background: #eee;
     text-align: center;
     display: flex;
-    padding: 12px 16px;
+    padding: .24rem .32rem;
     .search{
-        text-indent: 10px;
-        height:30px;
+        text-indent: .2rem;
+        height:.64rem;
         flex:1;
+        font: .3rem/.5rem "";
     }
     .cancle{
-        width: 50px;
-        font: 16px/30px '';
+        width: .8rem;
+        font: .3rem/.7rem '';
         color:#555;
-        text-indent: 10px;
+        text-indent: .2rem;
     }
 }
 .searchul1{
     li{
         border-bottom: 1px solid #ddd;
-        padding: 3px 20px;
+        padding: .2rem .4rem;
         overflow: hidden;
+        height: .7rem;
     }
     .songname{
-        font: 16px/24px "";
+        font: .3rem/.36rem "";
     }
     .singer{
-         font: 14px/20px "";
+         font: .24rem/.36rem "";
          color: #666;
     }
     .icon{
         display: block;
         float: left;
-        width: 22px;
-        height: 20px;
+        width: .44rem;
+        height: .4rem;
         background: url(../common/img/musicicon.png) no-repeat left;
         background-position: 0 0;
-        background-size: 22px 30px;
-        margin: 10px 20px 0 0 ;
+        background-size: .44rem .6rem;
+        margin: .1rem .2rem 0 0 ;
     }
     .search-r{
         float: left;
